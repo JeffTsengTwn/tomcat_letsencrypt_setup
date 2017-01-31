3a5ac6d9e027b439b51d1f8e16fa8a2ef4419eb6
JeffZeng <jeff810123@gmail.com>
2017-02-01 06:15:32 +0800

    Change tomcat source

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # tomcat_letsencrypt_setup
 在tomcat上一步設定SSL使用let'sencrypt的憑證。
 #安裝方式
+	sudo sh
     sh tomcat_ssl.sh
-#注意事項
-#參考資料
--- tomcat_ssl.sh
+++ tomcat_ssl.sh
@@ -1,15 +1,11 @@
-sudo su
 apt-get update
 apt-get upgrade
 add-apt-repository ppa:webupd8team/java
 apt-get update
 apt-get install oracle-java8-installer
 apt-get install oracle-java8-set-default
+mv tomcat /opt
 cd /opt
-git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
-wget -O apache-tomcat-8.5.9.tar.gz http://ftp.mirror.tw/pub/apache/tomcat/tomcat-8/v8.5.9/bin/apache-tomcat-8.5.9.tar.gz
-tar -xvzf apache-tomcat-8.5.9.tar.gz
-mv apache-tomcat-8.5.9 tomcat
 cd letsencrypt
 read -p "Please input your domain name: " domain
 read -p "Please input your email:  " mail   
@@ -20,5 +16,3 @@ openssl pkcs12 -export -in fullchain.pem -inkey privkey.pem -out pkcs.p12 -name 
 read -p "Please input your password:  " password
 keytool -importkeystore -deststorepass ${password} -destkeypass ${password} -destkeystore keystore.jks -srckeystore pkcs.p12 -srcstoretype PKCS12 -srcstorepass ${password} -alias ${NAME}
 sed -i 's/<changeit>/'${password}'/g' /opt/tomcat/conf/server.xml
-
-
